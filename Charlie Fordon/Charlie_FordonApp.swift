fb3d2d3fb0d40d7bb4858342b10f0a847959cf0c
Charlie Fordon <fordoncf@gmail.com>
2021-11-05 08:26:00 -0400

    Initial Commit

---
+++ Charlie Fordon/Charlie_FordonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charlie_FordonApp.swift
+//  Charlie Fordon
+//
+//  Created by Charles Fordon on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Charlie_FordonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
